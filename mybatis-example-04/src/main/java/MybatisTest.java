d1b503685f2a91bd8f26c31cf0d6131a25795c80
lizhong <13590316596@163.com>
2023-08-24 05:26:19 +0800

    1：mybatis面向接口编程参数传递  2：mybatis面向接口编程接收返回值 --- 2023/08/23

--- mybatis-example-04/src/main/java/MybatisTest.java
+++ mybatis-example-04/src/main/java/MybatisTest.java
@@ -4,9 +4,11 @@ import org.apache.ibatis.session.SqlSession;
 import utils.DbOperatorUtil;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 class MybatisExample{
+    ////单个参数传递：通过Map传参， map的key要和#{}中的参数一致
     public void test1(){
         Map<String, Object> user = new HashMap<String, Object>();
         user.put("id", 1);
@@ -24,6 +26,7 @@ class MybatisExample{
         sqlSession.commit();
     }
 
+    //单个参数传递：通过实体类对象传参
     public void test2(){
         User user = new User();
         user.setId(2);
@@ -33,29 +36,71 @@ class MybatisExample{
         user.setPassword("2222222");
 
         SqlSession sqlSession = DbOperatorUtil.getSession();
-
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-
         mapper.InsertUserForObject(user);
 
         sqlSession.commit();
     }
 
+    //单个参数传递：基本类型传参
     public void test3(){
         SqlSession sqlSession = DbOperatorUtil.getSession();
-
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-
         User user = mapper.selectUserById(1);
+
         System.out.println(user);
     }
 
+    //多个参数传递
     public void test4(){
+        SqlSession sqlSession = DbOperatorUtil.getSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.updateUser("email-2-updatedV3", 2);
 
+        sqlSession.commit();
     }
 
+    //Map接收返回的单条数据
     public void test5(){
+        SqlSession sqlSession = DbOperatorUtil.getSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
 
+        //采用Map集合接收，mybatis会将数据库字段名做为map中key，字段值做value，如果SQL语句中使用了别名，则使用指定的别名做key
+        Map<String, Object> userMap = mapper.selectUserReceiveByMap(2);
+
+        System.out.println(userMap);
+    }
+
+    //Map接收返回的多条数据
+    public void test6(){
+        SqlSession sqlSession = DbOperatorUtil.getSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        //采用Map集合接收，mybatis会将数据库字段名做为map中key，字段值做value，如果SQL语句中使用了别名，则使用指定的别名做key
+        List<Map<String, Object>> userList = mapper.selectAllUserReceiveByListMap();
+
+        System.out.println(userList);
+    }
+
+    //Map<String, Map<String, Object>> 接收多条数据
+    public void test7(){
+        SqlSession sqlSession = DbOperatorUtil.getSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        //采用Map集合接收，mybatis会将数据库字段名做为map中key，字段值做value，如果SQL语句中使用了别名，则使用指定的别名做key
+        Map<String, Map<String, Object>> userMapMap = mapper.selectAllUserReceiveByMapMap();
+
+        System.out.println(userMapMap);
+    }
+
+
+    //单个参数传递：基本类型传参
+    public void test8(){
+        SqlSession sqlSession = DbOperatorUtil.getSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.selectUserByAge(2);
+
+        System.out.println(user);
     }
 }
 
@@ -65,6 +110,11 @@ public class MybatisTest {
         MybatisExample mybatisExample = new MybatisExample();
         //mybatisExample.test1();
         //mybatisExample.test2();
-        mybatisExample.test3();
+        //mybatisExample.test3();
+        //mybatisExample.test4();
+        //mybatisExample.test5();
+        //mybatisExample.test6();
+        //mybatisExample.test7();
+        mybatisExample.test8();
     }
 }
--- mybatis-example-04/src/main/java/mapper/UserMapper.java
+++ mybatis-example-04/src/main/java/mapper/UserMapper.java
@@ -2,7 +2,10 @@ package mapper;
 
 
 import entity.User;
+import org.apache.ibatis.annotations.MapKey;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
 import java.util.Map;
 
 /*
@@ -22,11 +25,54 @@ public interface UserMapper {
     public Integer InsertUserForMap(Map<String, Object> userAttrMap);
 
 
-    //单个参数传递：通过对象传参， map的key要和#{}中的参数一致
+    //单个参数传递：通过实体类对象传参
     public Integer InsertUserForObject(User user);
 
-    //单个参数传递：基本类型
+    //单个参数传递：基本类型传参
     public User selectUserById(Integer userId);
 
 
+    //多个参数传递
+    public Integer updateUser(@Param("email") String email, @Param("userId") Integer id);
+
+
+    //Map接收返回的单条数据
+    public Map<String, Object>  selectUserReceiveByMap(Integer userId);
+
+
+    //Map接收返回的多条数据
+    public List<Map<String, Object>> selectAllUserReceiveByListMap();
+
+    //Map<String, Map>接收返回的多条数据:使用List<Map>进行接收多条数据，对结果集中的数据进行查询，需要顺序遍历查询，不方便。
+    //为了方便查询，可以拿数据记录的id（主键）做key，由整条数据记录封装而成的Map作为value组成的Map<String, Map<String, Object>> 接收
+    @MapKey("id")
+    public Map<String, Map<String, Object>> selectAllUserReceiveByMapMap();
+
+
+
+    //resultMap 进行结果映射:  当查询的数据库的列名和 java 对象的属性名对应不上时，如user表的full_name字段，在User实体类的属性名称为fullName
+    public User selectUserByAge(Integer age);
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
